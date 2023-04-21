80b6920419ae2e34ea323ee663bea25d1f4a318b
isaac mcdaniel <ilmcdaniel17@gmail.com>
2023-04-21 11:36:07 -0700

    no login info yet

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# challenge14
+
+    #Challenge14
+
+    ## Description
+    this project takes a user's input and allows us to create a very simple blog style website. 
+
+    ## Table of Contents
+        * [Installation](#installation)
+        * [Usage](#usage)
+        * [Credits](#credits)
+        * [Tests](#tests)
+        * [Features](#features)
+        * [License](#license)
+
+    ---
+    ## Installation
+    Installation instructions: dompurify, ejs, express, jsdom, marked, method-override, mongoose, slugify, nodemon
+    ---
+    ## Usage
+    use npm i to install all dependencies, then npm run devStart to begin load on local to allow for testing in development environment.
+    ---
+    ## Credits
+    Credit to outside third party sources used in development: 
+    web dev simplified on youtube for the inspiration of this project (i followed a tutorial and coded along for the base of the project then modified) david fuka, and stack overflow
+    ---
+    ## Tests
+    none 
+    ---
+    ## Features
+    dynamically created cards that contain user input and allow for editing, deletion, and new creations with date stamps and a clean simple design
+    ---
+    ## License
+    License used for this project: MIT License
--- server.js
+++ server.js
@@ -1,23 +1,20 @@
-const express = require('express');
-const app = express();
-const articleRouter = require('./routes/articles');
-const mongoose = require('mongoose');
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
 
-mongoose.connect('mongodb://localhost/blog', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost/blog', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 const PORT = process.env.PORT || 5000;
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
-app.use(express.urlencoded({ extended: false}))
-
-app.get('/', function(req, res) {
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date(),
-        description: 'test description',
-
-    }]
-    res.render('articles/index',  { articles : articles });
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
 })
 
 app.use('/articles', articleRouter)
